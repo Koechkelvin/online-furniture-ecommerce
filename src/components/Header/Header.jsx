4a77a481b9ef6002609d63cff86c5b6c0325eb69
koechkelvin <koech9524@gmail.com>
2024-07-11 15:34:18 +0300

    - Added Redux store configuration. - Created cartSlice with initial state and addItem reducer. - Updated Header component to fix useSelector issue and add menuToggle function. - Modified ProductList component to fix eslint warning and improve rendering of ProductCard. - Updated package.json and package-lock.json files.
    Changes address Redux integration for managing cart state, component updates for functionality and linting improvements.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useRef, useEffect} from 'react';
 import { NavLink } from 'react-router-dom'; // Ensure this is from 'react-router-dom'
 import { Container, Row } from 'reactstrap';
 import './header.css';
@@ -6,6 +6,7 @@ import {motion} from 'framer-motion'
 
 import userIcon from '../../assets/images/user-icon.png';
 import logo from '../../assets/images/eco-logo.png';
+import { useSelector } from 'react-redux';
 const nav__links=[
   {
     path:'home',
@@ -25,7 +26,28 @@ const nav__links=[
 ]
 
 const Header = () => {
+  const headerRef = useRef(null);        
+  const totalQuantity =useSelector(state=> state.cart.totalQuantity)
+  const menuRef = useRef (null);
+  const stickyHeaderFunc =()=>{
+    window.addEventListener("scroll", () =>{
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop >80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else{
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+    useEffect(()=>{
+      stickyHeaderFunc();
+      return() => window.removeEventListener("scroll", stickyHeaderFunc);
+    });
+   const menuToggle =() => menuRef.current.classList.toggle ("active__menu");
+  }
   return (
+
     <header className="header">
       <Container>
         <Row>
@@ -38,32 +60,35 @@ const Header = () => {
               </div>
             </div>
             <div className="navigation"> 
-              <ul className="menu">
+              <motion.ul className="menu">
                {
                 nav__links.map((item, index) =>(
                   <li className='nav__item'key={index}> 
                     <NavLink to={item.path} className={(navClass)=>
                       navClass.isActive ? 'nav__active' : ""
-                    }>{item.display}</NavLink>
+                    }>{item.display}
+                    </NavLink>
                   </li>
                 ))
                }
-              </ul>
+              </motion.ul>
             </div>
             <div className='nav__icons'>
               <span className="fav__icon"><i className="ri-heart-line"></i>
-              <span className='badge'>1</span>
+              <span className='badge'>{totalQuantity}</span>
               </span> 
               <span className="cart__icon"><i className="ri-shopping-cart-line"></i>
-              <span className='badge'>1</span>
+              <span className='badge'>2</span>
               </span> 
               <span>
                 <motion.img whileTap={{scale: 1.2}}src={userIcon} 
-                alt="user icon" />
+                alt="" />
               </span>
             </div>
             <div className='mobile__menu'>
-              <span><i className="ri-menu-line"></i></span> 
+              <span> onClick ={menuToggle}
+                <i className="ri-menu-line"></i>
+                </span> 
             </div>
           </div>
         </Row>
--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -4,10 +4,24 @@ import {Col} from 'reactstrap';
 import '../../styles/ProductCard.css';
 import {motion} from "framer-motion";
 import {Link} from 'react-router-dom';
-
-
+import {useDispatch} from 'react-redux';
+import { cartActions } from '../../redux/slices/cartSlice';
+import { toast } from 'react-toastify';
 
 const ProductCard = ({item}) => {
+  const dispatch = useDispatch()
+  const addToCart=()=>{
+
+    dispatch(
+      cartActions.addItem({
+      id:item.id,
+      productName:item.productName,
+      price:item.price,
+      image:item.imgUrl,
+    })
+  );
+  toast.success('Product added successfully');
+  };
   return ( 
     <Col lg="3" md="4">
         <div className='product__item'>
@@ -22,7 +36,7 @@ const ProductCard = ({item}) => {
         </div>
         <div className='product__card-bottom d-flex align-items-center justify-content-between p-2'>
             <span className='price'>{item.price}</span>
-            <motion.span whileTap={{scale:1.2}}>
+            <motion.span whileTap={{scale:1.2}} onClick={addToCart}>
               <i className="ri-add-fill"></i>
               </motion.span>
         </div>
