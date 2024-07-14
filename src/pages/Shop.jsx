f325cfb2c69be623090bcbb7e99439e9789781cb
koechkelvin <koech9524@gmail.com>
2024-07-14 05:21:09 +0300

    merge

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import Helmet from '../components/Helmet/Helmet';
 import CommonSection from '../components/UI/CommonSection';
+import { Container, Row, Col } from "reactstrap";
+import '../styles/shop.css';
 
 const Shop = () => {
   return (
     <Helmet title='Shop'>
-       <CommonSection title="Products"/>
-     
+      <CommonSection title="Products"/>
+      <Container>
+        <Row>
+          <Col lg='3' md='3'>
+            <div className='filter__widgets'>
+              <select>
+                <option>Filter by Category</option>
+                <option value="sofa">Sofa</option>
+                <option value="chair">Chair</option>
+                <option value="couch">Couch</option>
+              </select>
+            </div>
+          </Col>
+          <Col lg="3" md="3">
+            <div className='filter__widgets'>
+              <select>
+                <option>Sort By</option>
+                <option value="ascending">Ascending</option>
+                <option value="descending">Descending</option>
+              </select>
+            </div>
+          </Col>
+          <Col lg="3" md="3">
+            <div className='search__box'>
+              <input type="text" placeholder="Search......"/>
+              <span>
+                <i className="ri-search-line"></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </Helmet>
   );
 };
