1ba38be6224ddd81e37fcef3e571dec239cc8612
koechkelvin <koech9524@gmail.com>
2024-07-15 10:45:09 +0300

    merge

--- src/assets/data/serviceData.js
+++ src/assets/data/serviceData.js
@@ -3,25 +3,25 @@ const serviceData = [
     icon: "ri-truck-line",
     title: "No-Cost Shipping",
     subtitle: "Enjoy free shipping on all furniture.",
-    bg: "#fdefe6",
+    bg: "#6699FF",
   },
   {
     icon: "ri-refresh-line",
     title: "Seamless Returns",
     subtitle: "Easy returns for hassle-free shopping!",
-    bg: "#ceebe9",
+    bg: "#6699FF",
   },
   {
     icon: "ri-secure-payment-line",
     title: "Safe Transactions",
     subtitle: "Secure payment for worry-free shopping!",
-    bg: "#e2f2b2",
+    bg: "#6699FF",
   },
   {
     icon: "ri-exchange-dollar-line",
     title: "Return Warranty",
     subtitle: "Money-back guarantee for your satisfaction!",
-    bg: "#d6e5fb",
+    bg: "#6699FF",
   },
 ];
 
